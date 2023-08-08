fb7651b0611a9c3dd136b3e9fe8410de11adab53
Ahmed shaban mohammed <109414595+ahmedshaban744@users.noreply.github.com>
2023-08-08 19:22:58 +0300

    Assignment1

---
+++ C_Programming/Assignment1/ass1/code2.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+
+void  main (void)
+{
+	int x,y,z;
+	printf("enter the number 1\t:");
+	scanf("%d",&x);
+	
+	
+	printf("enter the number 2\t:");
+	scanf("%d",&y);
+	
+	printf("enter the number 3\t:");
+	scanf("%d",&z);
+	
+	printf("num3 %d\nnum2 %d\nnum1 %d\n ",z,y,x);
+		
+	
+}
---
+++ C_Programming/Assignment1/ass1/code3.c
@@ -0,0 +1,22 @@
+#include  <stdio.h>
+
+void  main (void)
+{
+	int x,y,z;
+	printf("enter the number 1\t:");
+	scanf("%d",&x);
+	printf("enter the number 2\t:");
+	scanf("%d",&y);
+	z=x+y;
+	printf("x+y= %d \n",z);
+	z=x-y;
+	printf("x-y= %d \n",z);
+	z=x&y;
+	printf("x&y= %d \n",z);
+		z=x&y;
+	printf("x&y= %d \n",z);
+	z=x|y;
+	printf("x|y= %d \n",z);
+		z=x^y;
+	printf("x^y= %d \n",z);
+}
